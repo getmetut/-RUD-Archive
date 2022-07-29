60f62614d32029691d2d377804b43172dbd0d554
Игорь Медведев <40432336+getmetut@users.noreply.github.com>
2022-07-29 15:07:27 +0400

    Add files via upload

---
+++ src/test/java/com/archive/application/CRUDArchiveTests.java
@@ -0,0 +1,13 @@
+package com.archive.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CRUDArchiveTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
