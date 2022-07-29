77b26f3d6a56f3d565e25309242161d8f9371519
Игорь Медведев <40432336+getmetut@users.noreply.github.com>
2022-07-29 15:05:50 +0400

    Delete CRUDArchive directory

--- CRUDArchive/src/main/java/com/archive/application/repository/DocRepository.java
+++
@@ -1,7 +0,0 @@
-package com.archive.application.repository;
-
-import com.archive.application.model.Doc;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocRepository extends JpaRepository<Doc, Long> {
-}
